ab19bc007773ac686e320c2733ce28e80f8fdadf
rogedev <apteruno@gmail.com>
2020-11-25 03:33:13 -0600

    adding Products class

--- scripts/cart.php
+++ scripts/cart.php
@@ -1,9 +1,32 @@
 <?php
-$q = $_POST["quantity"];
-$n = $_POST["name"];
-$p = $_POST["price"];
-$total = $q*$p;
-echo $q;
-echo $n;
-echo $p;
+//array a contains the article atributes
+class Product{
+
+    public $name;
+    public $quantity;
+    public $price;
+    public $total;
+
+    public function __construct($name, $quantity=0, $price=0){
+        $this->name = $name;
+        $this->quantity = $quantity;
+        $this->price = $price;
+    }
+    public function getTotal(){
+        return $this->total = $this->quantity*$this->price;
+
+    }
+    public function getInfo(){
+        $info = "<h1>Informacion del producto:</h1>";
+        $info.= "Producto: ".$this->name;
+        $info.= "<br/>Cantidad: ".$this->quantity;
+        $info.= "<br/> Total: ".$this->getTotal();
+
+        return $info;
+    }
+
+}
+$a = new Product($_POST["name"],$_POST["quantity"],$_POST["price"]);
+echo $a->getInfo();
+
 ?>
